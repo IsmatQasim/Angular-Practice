9c99ed2cb211edd1e642902a7d901b1f86d82ba4
IsmatQasim <ismat6812@gmail.com>
2025-08-21 23:05:01 +0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import { UserDataService } from './service/user-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular16';
+  // name:string = ""
+  // age : number | null = null
+  // description : string = ""
+
+  // ---------------------------------------------
+
+  // count:number=0
+  // increment(){
+  //   this.count++;
+  // }
+  // decrement(){
+  //   this.count--;
+  // }
+
+    // ---------------------------------------------
+
+
+//   list: string[] = [];
+
+//   id: Number = 0;
+// addTask(input: HTMLInputElement) {
+//   if (input.value.trim() === '') {
+//     alert('Empty Task! , Write Something');
+//   } else {
+//     this.list.push(input.value);
+//     console.log(this.list);
+//     input.value = '';
+//   }
+// }
+//   deleteItem(index: number) {
+//     this.list.splice(index, 1);
+//     console.log(this.list);
+//   }
+
+// --------------------------------
+users:any
+constructor(private userData:UserDataService) {
+}
+
+ngOnInit() {
+  this.userData.users().subscribe((data) => {
+    console.log(this.users=data);
+    console.log(this.users)
+  })
+}
 }
